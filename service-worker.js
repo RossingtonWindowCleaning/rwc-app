20db92dfd4dde7e75ddfb5d1659f89a04f35ee38
RossingtonWindowCleaning <rossingtonwindowcleaning@gmail.com>
2026-03-31 14:46:17 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -58,7 +58,7 @@ self.addEventListener('notificationclick', function(event) {
 // PWA Caching
 // ============================================================
 
-const CACHE_NAME = 'rwc-v22';
+const CACHE_NAME = 'rwc-v23';
 const APP_SHELL = [
   './home.html',
   './payments.html',
